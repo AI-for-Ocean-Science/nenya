33352ad3233349715545146928de1c3bd3e5a523
profxj <xavier@ucolick.org>
2026-03-10 17:49:50 -0700

    sync

--- papers/InformationContent/Analysis/sync_to_Drive.sh
+++ papers/InformationContent/Analysis/sync_to_Drive.sh
@@ -1,4 +1,4 @@
 # Pk
 rclone sync Pk AIOcean:AI+RemoteSensing/InformationContent/Pk --verbose
 # pca
-rclone sync pca AIOcean:AI+RemoteSensing/InformationContent/pca --verbose
+#rclone sync pca AIOcean:AI+RemoteSensing/InformationContent/pca --verbose
